99a3dcf1e41404d1357dc9d1a974ec63383db358
libretroadmin <reallibretroretroarch@gmail.com>
2022-06-25 17:34:43 +0200

    - Remove unused minizip - Remove unused easing.h/lru_cache.h headers

--- src/common/bitutils.h
+++ src/common/bitutils.h
@@ -10,25 +10,18 @@ template<typename T>
 ALWAYS_INLINE unsigned CountLeadingZeros(T value)
 {
 #ifdef _MSC_VER
+  unsigned long index;
   if constexpr (sizeof(value) >= sizeof(u64))
-  {
-    unsigned long index;
     _BitScanReverse64(&index, ZeroExtend64(value));
-    return static_cast<unsigned>(index) ^ static_cast<unsigned>((sizeof(value) * 8u) - 1u);
-  }
   else
-  {
-    unsigned long index;
     _BitScanReverse(&index, ZeroExtend32(value));
-    return static_cast<unsigned>(index) ^ static_cast<unsigned>((sizeof(value) * 8u) - 1u);
-  }
+  return static_cast<unsigned>(index) ^ static_cast<unsigned>((sizeof(value) * 8u) - 1u);
 #else
   if constexpr (sizeof(value) >= sizeof(u64))
     return static_cast<unsigned>(__builtin_clzl(ZeroExtend64(value)));
   else if constexpr (sizeof(value) == sizeof(u32))
     return static_cast<unsigned>(__builtin_clz(ZeroExtend32(value)));
-  else
-    return static_cast<unsigned>(__builtin_clz(ZeroExtend32(value))) & static_cast<unsigned>((sizeof(value) * 8u) - 1u);
+  return static_cast<unsigned>(__builtin_clz(ZeroExtend32(value))) & static_cast<unsigned>((sizeof(value) * 8u) - 1u);
 #endif
 }
 
@@ -37,22 +30,17 @@ template<typename T>
 ALWAYS_INLINE unsigned CountTrailingZeros(T value)
 {
 #ifdef _MSC_VER
+  unsigned long index;
   if constexpr (sizeof(value) >= sizeof(u64))
   {
-    unsigned long index;
     _BitScanForward64(&index, ZeroExtend64(value));
     return index;
   }
-  else
-  {
-    unsigned long index;
-    _BitScanForward(&index, ZeroExtend32(value));
-    return index;
-  }
+  _BitScanForward(&index, ZeroExtend32(value));
+  return index;
 #else
   if constexpr (sizeof(value) >= sizeof(u64))
     return static_cast<unsigned>(__builtin_ctzl(ZeroExtend64(value)));
-  else
-    return static_cast<unsigned>(__builtin_ctz(ZeroExtend32(value)));
+  return static_cast<unsigned>(__builtin_ctz(ZeroExtend32(value)));
 #endif
 }
--- src/common/minizip_helpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "unzip.h"
-
-namespace MinizipHelpers {
-
-unzFile OpenUnzMemoryFile(const void* memory, size_t memory_size);
-unzFile OpenUnzFile(const char* filename);
-
-} // namespace MinizipHelpers
