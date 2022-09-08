ab3da208b8dc5ba2c0273caa34e7344b3469a6b3
LibretroAdmin <reallibretroretroarch@gmail.com>
2022-09-08 08:48:47 +0200

    Simplify CountTrailingZeroes

--- src/common/bitutils.h
+++ src/common/bitutils.h
@@ -32,11 +32,9 @@ ALWAYS_INLINE unsigned CountTrailingZeros(T value)
 #ifdef _MSC_VER
   unsigned long index;
   if constexpr (sizeof(value) >= sizeof(u64))
-  {
     _BitScanForward64(&index, ZeroExtend64(value));
-    return index;
-  }
-  _BitScanForward(&index, ZeroExtend32(value));
+  else
+    _BitScanForward(&index, ZeroExtend32(value));
   return index;
 #else
   if constexpr (sizeof(value) >= sizeof(u64))
