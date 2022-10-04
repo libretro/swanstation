1b12727c91f441f203a6d067ec123f181cf35360
DarthMew <89034053+DarthMew@users.noreply.github.com>
2022-10-04 02:57:52 +0200

    Default to "auto" for the GPU renderer on Windows

--- src/libretro/libretro_core_options.h
+++ src/libretro/libretro_core_options.h
@@ -499,11 +499,7 @@ struct retro_core_option_v2_definition option_defs_us[] = {
          { "Software", "Software" },
          { NULL, NULL },
       },
-#ifdef WIN32
-      "D3D11"
-#else
       "Auto"
-#endif
    },
    {
       "duckstation_GPU.ResolutionScale",
