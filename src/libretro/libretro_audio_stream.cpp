d413cd03bfa9bf907cf4ac6124454c7b687ea760
DarthMew <adriesvd@gmail.com>
2023-08-18 18:40:15 +0200

    Restore old audio hook

--- src/libretro/libretro_audio_stream.cpp
+++ src/libretro/libretro_audio_stream.cpp
@@ -11,15 +11,16 @@ LibretroAudioStream::LibretroAudioStream() = default;
 
 LibretroAudioStream::~LibretroAudioStream() = default;
 
-void LibretroAudioStream::FramesAvailable()
+void LibretroAudioStream::FramesAvailable() 
 {
-  std::array<SampleType, MaxSamples> output_buffer;
-  u32 total_samples = 0;
-  while (const auto num_samples = m_buffer.GetContiguousSize()) {
-    const auto write_pos = output_buffer.begin() + total_samples;
-    std::copy_n(m_buffer.GetReadPointer(), num_samples, write_pos);
+  for (;;)
+  {
+    const u32 num_samples = m_buffer.GetContiguousSize();
+    if (num_samples == 0)
+      break;
+
+    const u32 num_frames = num_samples / AUDIO_CHANNELS;
+    g_retro_audio_sample_batch_callback(m_buffer.GetReadPointer(), num_frames);
     m_buffer.Remove(num_samples);
-    total_samples += num_samples;
   }
-  g_retro_audio_sample_batch_callback(output_buffer.data(), total_samples / AUDIO_CHANNELS);
 }
