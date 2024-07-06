ac0ef935463be1ed18e266d740c3e5bb1dd4d663
DarthMew <89034053+DarthMew@users.noreply.github.com>
2024-07-06 21:29:24 +0200

    Increase number of pads

--- src/libretro/libretro_host_interface.cpp
+++ src/libretro/libretro_host_interface.cpp
@@ -284,8 +284,8 @@ void LibretroHostInterface::retro_set_environment()
   };
 
   static const struct retro_controller_info ports[] = {
-   	  { pads, 7 },
-   	  { pads, 7 },
+   	  { pads, 8 },
+   	  { pads, 8 },
    	  { pads_mt, 6 },
    	  { pads_mt, 6 },
    	  { pads_mt2, 5 },
